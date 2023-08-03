d9e470671d6ca243339bd239b6ff0979dcbe13c3
peishuangheng <peishuangeng@hualala.com>
2023-08-03 18:23:53 +0800

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+import { Link } from "react-router-dom";
+import { useState, useEffect, useCallback } from "react";
+const { Header, Content, Footer } = Layout;
 
-function App() {
+const MICROAPPS = [
+  { key: "/home", label: "主应用" },
+  { key: "/react", label: "react应用" },
+  { key: "/vue", label: "vue应用" },
+];
+
+const App = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  const [breadcrumbList, setBreadcrumbList] = useState([""]);
+  const popstateHandler = useCallback(() => {
+    setBreadcrumbList(window.location.pathname.split("/"));
+  }, []);
+  useEffect(() => {
+    window.addEventListener("popstate", popstateHandler);
+    return () => {
+      window.removeEventListener("popstate", popstateHandler);
+    };
+  }, [popstateHandler]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout className="layout">
+      <Header
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          style={{
+            flex: 1,
+          }}
+          mode="horizontal"
+          defaultSelectedKeys={["home"]}
+          items={MICROAPPS.map((_) => {
+            const key = _.key === "/home" ? "/" : _.key;
+            const Label = <Link to={key}>{_.label}</Link>;
+            // const Label = _.label;
+            return {
+              key: _.key,
+              label: Label,
+            };
+          })}
+          onClick={({ key }) => {
+            // history.push(key==="home" ?"":key)
+            // history.replace(key==="home" ?"":key)
+          }}
+        />
+      </Header>
+      <Content
+        style={{
+          padding: "0 50px",
+        }}
+      >
+        <Breadcrumb
+          style={{
+            margin: "16px 0",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {breadcrumbList.map((item, index) => {
+            if ((window.location.pathname === "/"))
+              return <Breadcrumb.Item key={index}>Home</Breadcrumb.Item>;
+            return (
+              <Breadcrumb.Item key={index}>
+                {item === ""
+                  ? "Home"
+                  : item.charAt(0).toUpperCase() + item.slice(1)}
+              </Breadcrumb.Item>
+            );
+          })}
+        </Breadcrumb>
+        <div
+          className="site-layout-content"
+          style={{
+            background: colorBgContainer,
+          }}
+        >
+          <div id="container">{/* <Outlet /> */}</div>
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        <div>powered by shaokaodashi</div>
+      </Footer>
+    </Layout>
   );
-}
-
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  registerMicroApps, // 注册应用
+  start, // 开启方法
+} from "qiankun";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//  const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element:<App/> ,
+//     errorElement:<App/>
+//     // children:[
+//     //   {
+//     //     path: "/react",
+//     //     element: <div id='container'>123</div>,
+//     //   },
+//     //   {
+//     //     path: "/vue",
+//     //     element: <div ></div>,
+//     //   },
+//     // ]
+//   }
+// ]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <App></App>
+  </BrowserRouter>
+  // <RouterProvider  router={router} fallbackElement={<App/>}/>
 );
 
+const apps = [
+  {
+    name: "vue-web-app", // 应用名字
+    entry: "//localhost:8082", // 默认会加载这个html 解析里面的js(这里使用的fetch) 动态的执行(子应用必须支持跨域)
+    container: "#container", // 容器名 挂载元素
+    activeRule: "/vue", // 激活路径 激活规则(当访问/vue 就把这个应用挂载到#vue上)
+    props: { a: 1 },
+  },
+  {
+    name: "react-web-app",
+    entry: "//localhost:8081",
+    container: "#container",
+    activeRule: "/react",
+  },
+];
+registerMicroApps(apps); // 注册应用
+start({
+  prefetch: false, // 取消预加载
+}); // 开启
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
