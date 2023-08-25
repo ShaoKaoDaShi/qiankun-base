6d6d88adb4aff8997ffbedf82e3a364cba241568
peishuangheng <peishuangeng@hualala.com>
2023-08-25 23:25:17 +0800

    feat vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import * as rrweb from 'rrweb';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define:{
-    "process.env.not_qiankun":process.env.BASE_ENV
-  },
-  plugins: [react()],
-  server:{
-    proxy:{
-      '/api': {
-        target:  "http://localhost:3001",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    }
-  },
+    define: {
+        "process.env.not_qiankun": process.env.BASE_ENV,
+    },
+    plugins: [react()],
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:3001",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+        },
+    },
 
-  build:{rollupOptions:{output:{manualChunks:{
-    lodash: ['lodash'],
-    rrweb: ['rrweb'],
-  }}}}
-})
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    lodash: ["lodash"],
+                    rrweb: ["rrweb"],
+                },
+            },
+        },
+    },
+});
